89e19861a48b57867a903be6e2820a244426d26c
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-08-11 11:27:01 +0300

    new comparable graphics

--- src/analyze_results.py
+++ src/analyze_results.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import argparse
 from pathlib import Path
+import os
 
 '''
 В этом скрипте мы анализируем результаты классификации
@@ -10,8 +11,10 @@ from pathlib import Path
 графики для разных метрик модели
 '''
 
-#TODO: добавить больше метрик классификации и графиков, добавить сохранение диаграм
+image_save_path = Path('./images')
 
+#! Поскольку в общем случае в датафрейме у нас есть метки моделей (использовалась ли генерация или нет)
+#! Нужно заранее отфильтровать нужную часть датафрейма
 def make_simple_diagram(df):
     # Создаем столбчатую диаграмму
     plt.figure(figsize=(10, 6))  # Устанавливаем размер фигуры
@@ -29,18 +32,18 @@ def make_simple_diagram(df):
     plt.tight_layout()  # Обеспечивает, что все элементы диаграммы помещаются в область отображения
     plt.show()    
 
-def make_compared_diagram(df):
+def make_compared_diagram_f1(df, save_image=False):
     # Создаем столбчатую диаграмму с группировкой
     plt.figure(figsize=(10, 6))
     sns.barplot(
         x='model_name', 
         y='label_1_f1', 
-        #! Здесь может быть прикол. Мы в датафрейме храним маркер в числовом виде, а по идее он дожен подаваться в логическом виде.
-        #! Не знаю, произойдет ли приведение типов под капотом.
         hue='use_generation',  
         data=df,
         palette={True: 'skyblue', False: 'salmon'}  # Цвета для наглядности
     )
+
+    plt.xticks(rotation=45, ha="right")
     
     plt.title('Сравнение качества моделей: С использованием синтетических данных и без')
     plt.xlabel('Модель')
@@ -48,10 +51,67 @@ def make_compared_diagram(df):
     plt.legend(title='Маркер', labels=['С синтетическими данными', 'Без синтетических данных'])
     plt.grid(axis='y', linestyle='--', alpha=0.7)
 
-    plt.show()
+    if not save_image:
+        plt.show()
+    else:
+        assert(image_save_path.exists())
+        plt.savefig(f'{image_save_path}/compare_model_barplot_f1.png')
+
+#! Здесь используется precision именно для метки 1
+def make_compared_diagram_precision(df, save_image=False):
+    # Создаем столбчатую диаграмму с группировкой
+    plt.figure(figsize=(10, 6))
+    sns.barplot(
+        x='model_name',
+        y='label_1_precision',
+        hue='use_generation',
+        data=df,
+        palette={True: 'skyblue', False: 'salmon'}  # Цвета для наглядности
+    )
+
+    plt.xticks(rotation=45, ha="right")
+    
+    plt.title('Сравнение качества моделей: С использованием синтетических данных и без')
+    plt.xlabel('Модель')
+    plt.ylabel('Precision')
+    plt.legend(title='Маркер', labels=['С синтетическими данными', 'Без синтетических данных'])
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    if not save_image:
+        plt.show()
+    else:
+        assert(image_save_path.exists())
+        plt.savefig(f'{image_save_path}/compare_model_barplot_precision.png')
+
+#! Здесь используется precision именно для метки 1
+def make_compared_diagram_recall(df, save_image=False):
+    # Создаем столбчатую диаграмму с группировкой
+    plt.figure(figsize=(10, 6))
+    sns.barplot(
+        x='model_name',
+        y='label_1_recall',
+        hue='use_generation',
+        data=df,
+        palette={True: 'skyblue', False: 'salmon'}  # Цвета для наглядности
+    )
+
+    plt.xticks(rotation=45, ha="right")
+
+    plt.title('Сравнение качества моделей: С использованием синтетических данных и без')
+    plt.xlabel('Модель')
+    plt.ylabel('Recall')
+    plt.legend(title='Маркер', labels=['С синтетическими данными', 'Без синтетических данных'])
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    if not save_image:
+        plt.show()
+    else:
+        assert(image_save_path.exists())
+        plt.savefig(f'{image_save_path}/compare_model_barplot_recall.png')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--filepath', type=str, default='./evaluation_report.csv', help='set path to csv file aith results')
+parser.add_argument('-s', '--save', action='store_true', help='if we need to save images')
 args = parser.parse_args()
 
 filepath = Path(args.filepath)
@@ -59,6 +119,13 @@ assert(filepath.exists())
 
 df = pd.read_csv(filepath.absolute())
 
-make_simple_diagram(df)
+if args.save:
+    if not image_save_path.exists():
+        os.mkdir(image_save_path.absolute())
 
-make_compared_diagram(df)
+# Строим графики для моделей без синтетики
+make_simple_diagram(df[df['use_generation'] == 0])
+
+make_compared_diagram_f1(df, save_image=args.save)
+make_compared_diagram_precision(df, save_image=args.save)
+make_compared_diagram_recall(df, save_image=args.save)
